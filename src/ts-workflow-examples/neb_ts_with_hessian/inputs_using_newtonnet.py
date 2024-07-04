9e2f755262c57fed6c6b028a1880b74a0b12b260
Anup Kumar <anukumar94663@gmail.com>
2024-07-04 05:36:18 -0700

    Added an input file for neb ts hess nn

---
+++ src/ts-workflow-examples/neb_ts_with_hessian/inputs_using_newtonnet.py
@@ -0,0 +1,8 @@
+[paths]
+inputs_dir = "/global/cfs/cdirs/m2834/kumaranu/molecules_kinbotprod_renamed"
+model_path = "/global/u2/k/kumaranu/Documents/NewtonNet/example/predict/training_52/models/best_model_state.tar"
+settings_path = "/global/u2/k/kumaranu/Documents/NewtonNet/example/predict/training_52/run_scripts/config0.yml"
+launchpad_file = "/global/homes/k/kumaranu/fw_config/my_launchpad.yaml"
+
+[run]
+tag = "jul1y24c"
