3833b0d2d7f4240d35aed78078c3df687b4d6941
Anup Kumar <anukumar94663@gmail.com>
2024-07-08 23:05:46 -0700

    Added a newtonnet call inside a mace test function just to test if the newtonnet ase calculator works inside GitHub actions.

--- tests/geodesic_ts_with_hessian/test_using_mace.py
+++ tests/geodesic_ts_with_hessian/test_using_mace.py
@@ -34,3 +34,17 @@ def test_geodesic_ts_hess_irc_mace(setup_test_environment):
 
     # Assertions
     assert len(jobs) == 4
+    from newtonnet.utils.ase_interface import MLAseCalculator
+
+    settings.NEWTONNET_MODEL_PATH = project_root / "tests" / "best_model_state.tar"
+    settings.NEWTONNET_CONFIG_PATH = project_root / "tests" / "config0.yml"
+    mlcalculator = MLAseCalculator(
+        model_path=settings.NEWTONNET_MODEL_PATH,
+        settings_path=settings.NEWTONNET_CONFIG_PATH,
+        hess_method='autograd',
+        disagreement='std',
+        device='cpu',
+    )
+    mlcalculator.calculate(reactant)
+    print('Done with energy calc!!!!!')
+    
